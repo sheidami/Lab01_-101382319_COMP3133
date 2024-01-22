8c26156cf6bb22010a3109d71e704eb62844a1dd
Sheida Moazeni <sheydami98@gmail.com>
2024-01-21 20:51:31 -0500

    Modified version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 const output = [];
 
 // Function to write data to a file
-function writeToFile(fileName, data) {
+function writeFile(fileName, data) {
     const stream = fs.createWriteStream(fileName, { flags: 'w' });
     // column name
     stream.write('country,year,population\n');  
@@ -15,6 +15,7 @@ function writeToFile(fileName, data) {
 ['canada.txt', 'usa.txt'].forEach(file => {
     if (fs.existsSync(file)) {
         fs.unlinkSync(file);
+        console.log('Files have been successfully deleted!');
     }
 });
 
@@ -24,8 +25,8 @@ fs.createReadStream('input_countries.csv')
     .on('end', () => {
         const canada_Data = output.filter(row => row.country.toLowerCase() === 'canada');
         const usa_Data = output.filter(row => row.country.toLowerCase() === 'united states');
-        writeToFile('canada.txt', canada_Data);
-        writeToFile('usa.txt', usa_Data);
+        writeFile('canada.txt', canada_Data);
+        writeFile('usa.txt', usa_Data);
 
         console.log('Files have been successfully updated!');
     });
